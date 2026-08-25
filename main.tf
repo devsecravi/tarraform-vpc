aabbd63eec7e876705fa6fa42ad921d024373af7
unknown <dasariravid145@gmail.com>
2026-08-25 08:37:14 +0530

    terraform

--- main.tf
+++ main.tf
@@ -152,3 +152,16 @@ resource "aws_nat_gateway" "main" {
   depends_on = [aws_internet_gateway.main]
 }
 
+resource "aws_route" "private" {
+
+        route_table_id            = aws_route_table.private.id
+        destination_cidr_block    = "0.0.0.0/0"
+        nat-gateway-id = aws_nat_gateway.main.id
+}
+
+resource "aws_route" "database" {
+
+        route_table_id            = aws_route_table.database.id
+        destination_cidr_block    = "0.0.0.0/0"
+        nat-gateway-id = aws_nat_gateway.main.id
+}
